91eb541a378149cccf52b3be2e8c1a851c2364b9
ashwin vivegan <ashwinjegan6@gmail.com>
2025-07-15 14:59:39 +0530

    first java

---
+++ Attendance/School/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the School Attendance System");
+        System.out.println("Session 1: Project Initialization and orientation complete");
+    }
+}
